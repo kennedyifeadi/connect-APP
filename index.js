702e11ca4a902b43814b46702ee22242c110e7e4
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-03-10 01:07:34 +0100

    Set up server and connected Database

---
+++ index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const session = require('express-session');
+const MongoStore = require("connect-mongo");
+
+
+const connectDB = require("./server/config/db");
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Connect to DB
+connectDB();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+    session({
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: true,
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+      }),
+      cookie: { 
+          maxAge: 1209600000,
+          secure: true
+      }
+    })
+  );
+  
+app.use(express.static("public"));
+
+// Templating Engine
+
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./server/routes/main"));
+app.use("/user", require("./server/routes/user"));
+app.use("/auth", require("./server/routes/auth"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
